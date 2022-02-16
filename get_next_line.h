35d9cb783490d4a87607f8ec2bb4f189d40cd40c
Larakh88 <88907380+Larakh88@users.noreply.github.com>
2022-02-16 15:30:38 +0400

    Add files via upload

--- get_next_line.h
+++ get_next_line.h
@@ -5,9 +5,20 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: lel-khou <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/02/09 10:16:35 by lel-khou          #+#    #+#             */
-/*   Updated: 2022/02/09 10:16:39 by lel-khou         ###   ########.fr       */
+/*   Created: 2022/02/16 11:03:45 by lel-khou          #+#    #+#             */
+/*   Updated: 2022/02/16 15:26:29 by lel-khou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+#include <unistd.h>
+#include <stdlib.h>
 
+char	*get_next_line(int fd);
+void	*ft_calloc(size_t n, size_t size);
+size_t	ft_strlen(const char *str);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
+
+#endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -5,9 +5,68 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: lel-khou <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/02/09 10:16:02 by lel-khou          #+#    #+#             */
-/*   Updated: 2022/02/09 10:16:04 by lel-khou         ###   ########.fr       */
+/*   Created: 2022/02/16 11:03:57 by lel-khou          #+#    #+#             */
+/*   Updated: 2022/02/16 15:29:34 by lel-khou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
 
+void	*ft_calloc(size_t n, size_t size)
+{
+	void	*ptr;
+	size_t	i;
+
+	i = 0;
+	ptr = (void *)malloc(n * size);
+	if (!ptr)
+		return (0);
+	while (i < n * size)
+	{
+		((unsigned char *)ptr)[i] = 0;
+		i++;
+	}
+	return (ptr);
+}
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (!str)
+	{
+		i++;
+		str++;
+	}
+	return (i);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*str;
+	size_t	i;
+
+	i = 0;
+	if (!s)
+		return (0);
+	if (len > ft_strlen(s))
+		len = ft_strlen(s);
+	str = malloc(sizeof(char) * len + 1);
+	if (!str)
+		return (0);
+	while (i < len && s[start] != 0)
+	{
+		str[i] = s[start];
+		i++;
+		start++;
+	}
+	str[i] = 0;
+	return (str);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+
+}
+//strchr
